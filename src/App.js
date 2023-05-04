cad1841c4b00a8f32097544d6b7ccc99801c7e91
Agustin <85139943+Lylhium@users.noreply.github.com>
2023-05-04 02:44:10 -0300

    commit working on pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
+import logo from "./logo.svg";
 import "./App.css";
 //components
 import { Navbar } from "./components/Navbar/Navbar";
-//import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import PanelClima from "./components/PanelClima/PanelClima";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <PanelClima />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<PanelClima />} />
+          <Route path="/about" element={<h1>About Us </h1>} />
+          <Route path="*" element={<h1>Page Not Found</h1>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/FormularioClima.js/FormClima.js
+++ src/components/FormularioClima.js/FormClima.js
@@ -1,31 +1,38 @@
-import React , {useState} from 'react'
+import React, { useState } from "react";
 //css
-import './FormClima.css'
+import "./FormClima.css";
 //icon-img
-import searchIcon from '../../assets/search.png'
+import searchIcon from "../../assets/search.png";
 
-const FormClima = ({newLocation}) => {
-    const [ ciudad, setCiudad ] = useState('');
+const FormClima = ({ newLocation }) => {
+  const [ciudad, setCiudad] = useState("");
 
-    const onSubmit = (e) => {
-        e.preventDefault();
-        console.log({ciudad});
-        if(ciudad == '' || !ciudad) return;
-          // en el caso que se incluyo una ciudad :
-          newLocation(ciudad);
-    }
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log({ ciudad });
+    // eslint-disable-next-line eqeqeq
+    if (ciudad == "" || !ciudad) return;
+    // en el caso que se incluyo una ciudad :
+    newLocation(ciudad);
+  };
 
-    return (
-    <div className='search'> 
-       <form className='search-bar' onSubmit={onSubmit}>
-          <div className='input-grupo '>
-             <input type="text" className='form-control' placeholder='Buscar' onChange={(e)=> setCiudad(e.target.value)}/>
-          <button className='boton-input input-group-text' type='submit'><img src={searchIcon}/></button> 
-          </div> 
-       </form> 
+  return (
+    <div className="search">
+      <form className="search-bar" onSubmit={onSubmit}>
+        <div className="input-grupo ">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Buscar"
+            onChange={(e) => setCiudad(e.target.value)}
+          />
+          <button className="boton-input input-group-text" type="submit">
+            <img src={searchIcon} />
+          </button>
+        </div>
+      </form>
     </div>
+  );
+};
 
-    );
-}
-
-   export default FormClima; 
+export default FormClima;
