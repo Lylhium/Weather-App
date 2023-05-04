f2fcf565b29bd2a3be897713db144d4da593f8cc
Agustin <85139943+Lylhium@users.noreply.github.com>
2023-05-04 02:21:15 -0300

    commit2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 //components
-import { Navbar } from './components/Navbar/Navbar';
-import { Routes, Route, BrowserRouter} from 'react-router-dom'
-import PanelClima from './components/PanelClima/PanelClima';
+import { Navbar } from "./components/Navbar/Navbar";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import PanelClima from "./components/PanelClima/PanelClima";
 
 function App() {
-return (
-  <div className="App">
-    <BrowserRouter>
-     <Navbar/>
-      <Routes>
-         <Route path='/' element={ <PanelClima/> } />
-         <Route path='/weather' element={ <h1> Pagina de Clima </h1> } />
-         <Route path='/about' element={ <h1>About Us </h1> }  />
-         <Route path='*' element={ <h1>Page Not Found</h1>}/>
-      </Routes>
-    </BrowserRouter>
-  </div>
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/weather" element={<PanelClima />} />
+          <Route path="/about" element={<h1>About Us </h1>} />
+          <Route path="*" element={<h1>Page Not Found</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
